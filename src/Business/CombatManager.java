096303ef1eac9bce6ebc816e99f4067fde0131c2
Aralo03 <alex.ramal@students.salle.url.edu>
2024-11-27 12:02:08 +0100

    tot

--- src/Business/CombatManager.java
+++ src/Business/CombatManager.java
@@ -1,6 +1,7 @@
 package Business;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CombatManager {
 
@@ -42,17 +43,60 @@ public class CombatManager {
 
     public boolean checkKnockOut(double damage) {
         boolean knocked = false;
+        Random random = new Random();
+        double randomKnockOut;
+
+        randomKnockOut = random.nextInt(199) + 1;
+        if (randomKnockOut < damage){
+            knocked = true;
+        }
 
         return knocked;
     }
 
-    public double calculateDamage() {
-        double damage = 0;
+    public double calculateDamage(CombatMember attacker) {
+        double damage;
+        int attackerWeight;
+        double attackerDamage;
+        int weaponPower;
+
+
+        attackerWeight = attacker.getCharacter().getWeight();
+        weaponPower = attacker.getWeapon().getPower();
+        attackerDamage = attacker.getDamage();
+
+        damage = attackerWeight * (1 - attackerDamage);
+        damage = damage/10;
+        damage = damage + (weaponPower/20) + 18;
 
         return damage;
     }
 
-    public void updateStats() {}
+    public double calculateReducedDamage(double damage, CombatMember defender) {
+        double finalDamage;
+        double defenderDamage;
+        int defenderWeight;
+        int armor;
+
+        defenderDamage = defender.getDamage();
+        defenderWeight = defender.getCharacter().getWeight();
+        armor = defender.getArmor().getPower();
+
+        finalDamage = 200 * (1 - defenderDamage);
+        finalDamage = finalDamage/defenderWeight;
+        finalDamage = finalDamage + (armor/20);
+        finalDamage = finalDamage * 1.4;
+        finalDamage = damage - finalDamage;
+        finalDamage = finalDamage/100;
+
+        return finalDamage;
+    }
+
+    public void updateStats() {
+        for (int i = 0; i < combat.getTeamList().size(); i++){
+            combat.getTeamList().get(i);
+        }
+    }
 
     public void endCombat() {}
 
@@ -63,5 +107,35 @@ public class CombatManager {
             combat.createCombatMember(characterList.get(i), "Balanced", weaponList.get(i), armorList.get(i));
         }
         combat.setTeamList(teamFight);
+    }
+
+    public ArrayList<String> getWeaponList() {
+        ArrayList<String> weaponNameList = new ArrayList<>();
+
+        for (int i = 0; i < combat.getCombatMemberList().size(); i++){
+            weaponNameList.add(combat.getCombatMemberList().get(i).getWeapon().getName());
+        }
+
+        return weaponNameList;
+    }
+
+    public ArrayList<String> getArmorList() {
+        ArrayList<String> armorNameList = new ArrayList<>();
+
+        for (int i = 0; i < combat.getCombatMemberList().size(); i++){
+            armorNameList.add(combat.getCombatMemberList().get(i).getArmor().getName());
+        }
+
+        return armorNameList;
+    }
+
+    public ArrayList<Double> getDamageList() {
+        ArrayList<Double> damageNameList = new ArrayList<>();
+
+        for (int i = 0; i < combat.getCombatMemberList().size(); i++){
+            damageNameList.add(combat.getCombatMemberList().get(i).getDamage());
+        }
+
+        return damageNameList;
     }
 }
