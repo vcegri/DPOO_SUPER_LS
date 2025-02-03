bf129f19b41875facf5a06a99b13d7f1e564389d
vcegri <victor.cegri@gmail.com>
2025-02-03 11:15:21 +0100

    javadoc

--- src/Persistence/TeamDeserializer.java
+++ src/Persistence/TeamDeserializer.java
@@ -22,25 +22,19 @@ public class TeamDeserializer implements JsonDeserializer<Team> {
     @Override
     public Team deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
-        //JsonArray jsonArray = json.getAsJsonArray();
-        //ArrayList<Team> teamList = new ArrayList<>();
 
-        //for (int i = 0; i < jsonArray.size(); i++) {
-          //  JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
-            String name = jsonObject.get("name").getAsString();
-            ArrayList<TeamMember> members = new ArrayList<>();
-            JsonArray membersArray = jsonObject.getAsJsonArray("members");
+        String name = jsonObject.get("name").getAsString();
+        ArrayList<TeamMember> members = new ArrayList<>();
+        JsonArray membersArray = jsonObject.getAsJsonArray("members");
 
-            for (JsonElement memberElement : membersArray) {
-                JsonObject reviewObject = memberElement.getAsJsonObject();
-                Long id = reviewObject.get("id").getAsLong();
-                String strategy = reviewObject.get("strategy").getAsString();
-                TeamMember member = new TeamMember(id, strategy);
-                //TeamMember member = context.deserialize(memberElement, TeamMember.class);
-                members.add(member);
-            }
-            //teamList.add(new Team(name, members));
-        //}
+        for (JsonElement memberElement : membersArray) {
+            JsonObject reviewObject = memberElement.getAsJsonObject();
+            Long id = reviewObject.get("id").getAsLong();
+            String strategy = reviewObject.get("strategy").getAsString();
+            TeamMember member = new TeamMember(id, strategy);
+            members.add(member);
+        }
+
         return new Team(name, members);
     }
 }
